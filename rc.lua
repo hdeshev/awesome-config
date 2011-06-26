c5aa05ffc59cc6829c5b7effe4a00e8d0a53b774
Hristo Deshev <hristo@deshev.com>
2011-06-26 18:00:32 +0300

    Smaller volume up/down step.

--- rc.lua
+++ rc.lua
@@ -247,8 +247,8 @@ global_keys = awful.util.table.join(
 
   -- sound & brightness
   awful.key({}, "XF86AudioMute", function () obvious.volume_alsa.mute(0, "Master")     end),
-  awful.key({}, "XF86AudioLowerVolume", function () obvious.volume_alsa.lower(0, "Master", 5) end),
-  awful.key({}, "XF86AudioRaiseVolume", function () obvious.volume_alsa.raise(0, "Master", 5) end),
+  awful.key({}, "XF86AudioLowerVolume", function () obvious.volume_alsa.lower(0, "Master", 1) end),
+  awful.key({}, "XF86AudioRaiseVolume", function () obvious.volume_alsa.raise(0, "Master", 1) end),
   awful.key({ modkey }, "F8", function () spawn("brightness down")                  end),
   awful.key({ modkey }, "F9", function () spawn("brightness up")                    end),
 
