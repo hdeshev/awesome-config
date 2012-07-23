b6136fba57573880256e2df3f511fd9071f648b9
Hristo Deshev <hristo@deshev.com>
2012-07-23 16:09:18 +0300

    Xubuntu settings on the laptop too.

--- hosts/laptop.sh
+++ hosts/laptop.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Dropbox with GUI
-sudo /etc/rc.d/dropboxd start
+#sudo /etc/rc.d/dropboxd start
 
 # Network Manager applet - connect to various networks (eth, wlan)
-if [ "$(pgrep wicd-client)" ]
-then
-  echo "wicd-gtk already running"
-else
-  wicd-gtk -t &
-fi
+#if [ "$(pgrep wicd-client)" ]
+#then
+#  echo "wicd-gtk already running"
+#else
+#  wicd-gtk -t &
+#fi
 
 # XFCE power manager - turn off monitor, hibernate on lid close
 if [ "$(pidof xfce4-power-manager)" ]
@@ -18,3 +18,18 @@ then
 else
   xfce4-power-manager &
 fi
+
+# Start the XFCE settings daemon to make GUI programs look good
+xfsettingsd --force
+
+# Ubuntu only.
+dropbox start -i &
+
+# Network Manager applet - connect to various networks (eth, wlan)
+if [ "$(pidof nm-applet)" ]
+then
+  echo "nm-applet already running"
+else
+  nm-applet &
+fi
+
