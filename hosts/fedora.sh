998c0b02259966e278a28da4fbe2571b47c8ca2d
Hristo Deshev <hristo@deshev.com>
2013-07-29 06:55:42 -0400

    Disable some of the XFCE utils.

---
+++ hosts/fedora.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Load nvidia X settings
+# nvidia-settings --load-config-only
+
+# Start the XFCE settings daemon to make GUI programs look good
+# xfsettingsd --force
+
+# Ubuntu only.
+# dropbox start -i &
+
+# Network Manager applet - connect to various networks (eth, wlan)
+if [ "$(pidof nm-applet)" ]
+then
+  echo "nm-applet already running"
+else
+  nm-applet &
+fi
+
+#VNC server - remote pair programming
+# if [ "$(pidof vino-server)" ]
+# then
+#   echo "vino-server already running"
+# else
+#   /usr/lib/vino/vino-server &
+# fi
+
