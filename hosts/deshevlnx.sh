dd17c7c204ec395298556affb1c11b99b39165d1
Hristo Deshev <hristo@deshev.com>
2015-05-12 10:37:42 +0300

    2 monitor setup

---
+++ hosts/deshevlnx.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Load nvidia X settings
+# nvidia-settings --load-config-only
+
+# Start the XFCE settings daemon to make GUI programs look good
+xfsettingsd --replace
+
+# Gnome keyring daemon
+gnome-keyring-daemon --daemonize --login
+
+# Start dropbox daemon
+# ~/.dropbox-dist/dropboxd &
+
+# Start the pCloud Sync client using our own wrapper script
+# ~/bin/pclsync
+
+# Network Manager applet - connect to various networks (eth, wlan)
+if [ "$(pidof nm-applet)" ]
+then
+  echo "nm-applet already running"
+else
+  nm-applet &
+fi
+
+#VNC server - remote pair programming
+# if [ "$(pidof vino-server)" ]
+# then
+#   echo "vino-server already running"
+# else
+#   /usr/lib/vino/vino-server &
+# fi
+
+# Apply arandr-generated config
+~/.screenlayout/2monitors.sh
